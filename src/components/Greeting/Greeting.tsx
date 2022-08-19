9cc9081a3a3309aa373a2445cdca77de6fa23971
Daniil Sharenkov <Milkiy174@gmail.com>
2022-08-20 02:06:00 +0300

    fix: remake of main page

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -6,7 +6,13 @@ export const Greeting: React.FC = () => {
     <main className={cn(styles.main)}>
       <section className={cn(styles.section)}>
         <h2 className={cn(styles.h2)}>Изучай английский вместе с RS-LANG</h2>
-        <button className={cn(styles.button, 'button')} type='button'>Начать путь</button>
+        <p className={cn(styles.description)}>
+          Проходи увлекательные игры, запоминай сложные слова и следи за своим
+          прогрессом.
+        </p>
+        <button className={cn(styles.button, 'button')} type="button">
+          Начать путь
+        </button>
       </section>
     </main>
   );
