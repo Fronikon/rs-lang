6bac4c9d1c60ed0272f02417951c939054b04642
InnokentyKedrov <kedr_club@mail.ru>
2022-08-24 09:29:49 +0300

    feat: adds eventCreateUser

--- src/components/Autorization/createUser.ts
+++ src/components/Autorization/createUser.ts
@@ -6,6 +6,11 @@ export interface IUser {
   password: string,
 }
 
+export let eventCreateUser = {
+  code: 0,
+  message: ''
+};
+
 export const createUser = async (user: IUser) => {
   const response = await fetch(`${baseUrl}/users`, {
     method: 'POST',
@@ -16,13 +21,25 @@ export const createUser = async (user: IUser) => {
     body: JSON.stringify(user)
   }).then (response => {
     switch (response.status) {
-    case 200: console.log('Вы успешно зарегистрировались.');
+    case 200:
+      eventCreateUser = {
+        code: 200,
+        message: 'Вы успешно зарегистрировались'
+      };
       return response.json();
-    case 422: console.log('Неверный логин или пароль.');
+    case 422:
+      eventCreateUser = {
+        code: 200,
+        message: 'Неверный логин или пароль'
+      };
       return response.status;
-    case 417: console.log('Пользователь с таким email уже зарегистрирован.');
+    case 417:
+      eventCreateUser = {
+        code: 200,
+        message: 'Пользователь с таким email уже зарегистрирован'
+      };
       return response.status;
     }
   }).then (data => data);
-  console.log('response: ', response);
+  return response;
 };
