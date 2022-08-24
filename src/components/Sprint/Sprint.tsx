62aadf1794ba377bcff9935689cfc9b519b4a6fb
Daniil Sharenkov <Milkiy174@gmail.com>
2022-08-24 05:37:44 +0300

    feat: end up result section layout

--- src/components/Sprint/Sprint.tsx
+++ src/components/Sprint/Sprint.tsx
@@ -61,8 +61,8 @@ const Sprint: React.FC = () => {
 
       <div className={cn(styles.result_game_wrapper)}>
         <h2 className={cn(styles.result)}>Результат</h2>
-        <div className={cn(styles.results_wrapper, 'wrong')}>
-          <div className={cn(styles.counter, 'dont_know')}>
+        <div className={cn(styles.results_wrapper, styles.wrong)}>
+          <div className={cn(styles.counter, styles.dont_know)}>
             <h3 className={cn(styles.counter_heading)}>Не знаю</h3>
             <div className={cn(styles.wrong_count)}>18</div>
           </div>
@@ -87,11 +87,41 @@ const Sprint: React.FC = () => {
               —
               <p className={cn(styles.rus_standart_word)}>утка</p>
             </div>
+            <div className={cn(styles.word)}>
+              <img
+                className={cn(styles.word_icon)}
+                src={Megaphone}
+                alt="megaphone"
+              />
+              <p className={cn(styles.eng_bold_word)}>duck</p>
+              —
+              <p className={cn(styles.rus_standart_word)}>утка</p>
+            </div>
+            <div className={cn(styles.word)}>
+              <img
+                className={cn(styles.word_icon)}
+                src={Megaphone}
+                alt="megaphone"
+              />
+              <p className={cn(styles.eng_bold_word)}>duck</p>
+              —
+              <p className={cn(styles.rus_standart_word)}>утка</p>
+            </div>
+            <div className={cn(styles.word)}>
+              <img
+                className={cn(styles.word_icon)}
+                src={Megaphone}
+                alt="megaphone"
+              />
+              <p className={cn(styles.eng_bold_word)}>duck</p>
+              —
+              <p className={cn(styles.rus_standart_word)}>утка</p>
+            </div>
           </div>
         </div>
         <div className={cn(styles.line)}></div>
-        <div className={cn(styles.results_wrapper, 'right')}>
-          <div className={cn(styles.counter, 'know')}>
+        <div className={cn(styles.results_wrapper, styles.right)}>
+          <div className={cn(styles.counter, styles.know)}>
             <h3 className={cn(styles.counter_heading)}>Знаю</h3>
             <div className={cn(styles.right_count)}>2</div>
           </div>
