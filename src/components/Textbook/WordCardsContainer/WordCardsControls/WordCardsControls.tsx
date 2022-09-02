0e45a38def6330b15af369505ade100fbb8326b9
Dmitry Beresnev <dmitrberesnev@gmail.com>
2022-09-02 20:51:08 +0300

    feat: add links to games on textbook

--- src/components/Textbook/WordCardsContainer/WordCardsControls/WordCardsControls.tsx
+++ src/components/Textbook/WordCardsContainer/WordCardsControls/WordCardsControls.tsx
@@ -18,9 +18,18 @@ const WordCardsControls: React.FC<PropsType> = (props) => (
       <PageSelect currentPage={props.currentPage} />
       {props.isLearnedCurrentPage && <div className={styles.statusPage}>✓</div>}
     </div>
-    {props.isLogin && <Link to={'hardwords'}>
-      <button className={cn(styles['link-on-hardwords'], 'button')}>Сложные слова</button>
-    </Link>}
+    {props.isLogin &&
+      <div className={styles['link-container']}>
+        <Link to={'/sprint'}>
+          <button className={cn('button')}>Спринт</button>
+        </Link>
+        <Link to={'/audio'}>
+          <button className={cn('button')}>Аудиовызов</button>
+        </Link>
+        <Link to={'hardwords'}>
+          <button className={cn(styles['link-on-hardwords'], 'button')}>Сложные слова</button>
+        </Link>
+      </div>}
   </div>
 );
 
