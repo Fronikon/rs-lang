5586d25feb29c63057650fe5add5f9cafd3c2973
Andrey <kedr_club@mail.ru>
2022-09-02 07:47:41 +0300

    feat: changed scroolbar's styles

--- src/components/AudioChallenge/QuestionPage/AudioEnd/AudioEnd.tsx
+++ src/components/AudioChallenge/QuestionPage/AudioEnd/AudioEnd.tsx
@@ -17,28 +17,31 @@ const AudioEnd: React.FC<ArraysType> = ({rightAnswerArray, wrongAnswerArray}) =>
       <Link to="/audio" className={cn(styles.audioEnd__closeButton)}></Link>
       <div className={cn(styles.audioEnd__container)}>
         <h2 className={cn(styles.audioEnd__title)}>Результат</h2>
-        <div>
+        <div className={cn(styles.audioEnd__wrapper)}>
           <div className={cn(styles.audioEnd__title_container)}>
             <h3 className={cn(styles.audioEnd__title_small)}>Не знаю</h3>
             <span className={cn(styles.audioEnd__span_wrong)}>{wrongAnswerArray.length}</span>
           </div>
-          <ul className={cn(styles.audioEnd__wrongList)}>
-            {wrongAnswerArray.map((el) => 
-              <WrongArray word={el.word} wordTranslate={el.wordTranslate} audio={el.audio} key={el.id} />)}
-          </ul>
+          <div className={cn(styles.audioEnd__wrongList_container)}>
+            <ul className={cn(styles.audioEnd__list)}>
+              {wrongAnswerArray.map((el) => 
+                <WrongArray word={el.word} wordTranslate={el.wordTranslate} audio={el.audio} key={el.id} />)}
+            </ul>
+          </div>
         </div>
-        <div>
+        <div className={cn(styles.audioEnd__wrapper)}>
           <div className={cn(styles.audioEnd__title_container)}>
             <h3 className={cn(styles.audioEnd__title_small)}>Знаю</h3>
             <span className={cn(styles.audioEnd__span_right)}>{rightAnswerArray.length}</span>
           </div>
-          <ul className={cn(styles.audioEnd__rightList)}>
-            {rightAnswerArray.map((el) => 
-              <RightArray word={el.word} wordTranslate={el.wordTranslate} audio={el.audio} key={el.id} />)}
-          </ul>
+          <div className={cn(styles.audioEnd__rightList_container)}>
+            <ul className={cn(styles.audioEnd__list)}>
+              {rightAnswerArray.map((el) => 
+                <RightArray word={el.word} wordTranslate={el.wordTranslate} audio={el.audio} key={el.id} />)}
+            </ul>
+          </div>
         </div>
         
-
       </div>
     </section>
   );
