9562748c5950676a3e54ad8149a4646eb695821e
Dmitry Beresnev <dmitrberesnev@gmail.com>
2022-09-13 14:31:03 +0300

    feat: add redux-devtools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,14 @@
 import { createStore, applyMiddleware, AnyAction } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk, { ThunkDispatch } from 'redux-thunk';
 import rootReducer from './../redux/reducers/rootReducer';
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
 
 export type StoreType = ReturnType<typeof rootReducer>
 export type AppDispatchType = ThunkDispatch<StoreType, [], AnyAction>
