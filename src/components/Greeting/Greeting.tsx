b718418215626e7d044bb9d8a03a050073de1b9f
Andrey <kedr_club@mail.ru>
2022-09-21 19:04:39 +0300

    feat: change greeting page layout

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -9,18 +9,20 @@ const Greeting: React.FC = () => {
   return (
     <main className={cn(styles.main, 'container')}>
       <div className={styles['section-wrapper']}>
-        <section className={cn(styles['start-section'])}>
-          <h2 className={cn(styles.title, 'title-page')}>Изучай английский вместе с RS-LANG</h2>
-          <p className={cn(styles.description)}>
+        <div className={cn(styles['start-section_wrapper'])}>
+          <section className={cn(styles['start-section'])}>
+            <h2 className={cn(styles.title, 'title-page')}>Изучай английский вместе с RS-LANG</h2>
+            <p className={cn(styles.description)}>
           Проходи увлекательные игры, пополняй словарный запас благодаря учебнику состоящему из 3600 слов.
-          </p>
-          <div className={styles.buttons}>
-            <Link to="/textbook">
-              <button className={cn(styles.button, 'button')} type="button">Начать</button>
-            </Link>
+            </p>
+            <div className={styles.buttons}>
+              <Link to="/textbook">
+                <button className={cn(styles.button, 'button')} type="button">Начать</button>
+              </Link>
             
-          </div>
-        </section>
+            </div>
+          </section>
+        </div>
         {!isLogin && <section className={cn(styles['register-section'])}>
           <h2 className={cn(styles.title, 'title-page')}>Учётная запись</h2>
           <p className={cn(styles.description)}>
